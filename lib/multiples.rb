dbc8858ded82e5eaf607d91a52ee6c4a472ee133
Schyler Hosler <schylerhosler@gmail.com>
2016-10-24 11:06:39 -0400

    Ruby practice with collecting multiples of 3 and/or 5... within range and finding sum all tests passing

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  num_arr = []
+  limit -= 1
+  while limit > 0 do
+    if limit % 3 == 0 || limit % 5 == 0
+      num_arr.push(limit)
+    end
+    limit -= 1
+  end
+  return num_arr.sort
+end
+
+def sum_multiples(limit)
+  num_arr = collect_multiples(limit)
+  result = 0
+  while num_arr.length > 0 do
+    result += num_arr.pop
+  end
+  return result
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    num_limit = self.limit
+    num_arr = []
+    num_limit -= 1
+    while num_limit > 0 do
+      if num_limit % 3 == 0 || num_limit % 5 == 0
+        num_arr.push(num_limit)
+      end
+      num_limit -= 1
+    end
+    return num_arr.sort
+  end
+
+  def sum_multiples
+    num_arr = self.collect_multiples
+    result = 0
+    while num_arr.length > 0 do
+      result += num_arr.pop
+    end
+    return result
+  end
+
+end
